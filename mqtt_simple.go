4244e024a7a074c88e40e4bf4dc5dfe7279cbabb
Konstantin Belyalov <arsenicus@mail.ru>
2018-09-15 16:50:53 -0700

    Initial version of MQTT simple library

---
+++ mqtt_simple.go
@@ -0,0 +1,67 @@
+package mqtt_simple
+
+import (
+	pmqtt "github.com/eclipse/paho.mqtt.golang"
+	"golang.org/x/net/context"
+)
+
+type MqttSimple struct {
+	client        pmqtt.Client
+	opts          *pmqtt.ClientOptions
+	subscriptions map[string][]chan string
+}
+
+func NewMqttSimple(broker, clientid, user, password string) *MqttSimple {
+
+	opts := pmqtt.NewClientOptions()
+	opts.AddBroker(broker)
+	opts.SetClientID(clientid)
+	opts.SetUsername(user)
+	opts.SetPassword(password)
+	opts.SetCleanSession(true)
+
+	return &MqttSimple{
+		opts:          opts,
+		subscriptions: make(map[string][]chan string),
+	}
+}
+
+func (b *MqttSimple) onMessage(client pmqtt.Client, msg pmqtt.Message) {
+	payload := string(msg.Payload())
+	// Broadcast message to all subscribers
+	if channels, ok := b.subscriptions[msg.Topic()]; ok {
+		for _, ch := range channels {
+			ch <- payload
+		}
+	}
+}
+
+func (b *MqttSimple) Start(ctx context.Context) error {
+
+	b.opts.SetDefaultPublishHandler(b.onMessage)
+
+	b.client = pmqtt.NewClient(b.opts)
+	if token := b.client.Connect(); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	// To be able to disconnect client whenever context is done
+	go func() {
+		<-ctx.Done()
+		b.client.Disconnect(100)
+	}()
+
+	return nil
+}
+
+func (b *MqttSimple) Subscribe(topic string, qos byte) (<-chan string, error) {
+	// Subscribe to given topic
+	if token := b.client.Subscribe(topic, qos, nil); token.Wait() && token.Error() != nil {
+		return nil, token.Error()
+	}
+
+	// Return channel to this topic
+	ch := make(chan string, 1)
+	b.subscriptions[topic] = append(b.subscriptions[topic], ch)
+	return ch, nil
+}
